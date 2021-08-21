5ce385ac5f53b8ff188142e5d1d9e43b4128e9b0
NatalyMora21 <tatamora21@gmail.com>
2021-08-21 16:37:41 -0500

    Created connection to database

---
+++ db/dbConnection.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"log"
+
+	"github.com/dgraph-io/dgo"
+	"github.com/dgraph-io/dgo/protos/api"
+	"google.golang.org/grpc"
+)
+
+func ConnectionDgraph() *dgo.Dgraph {
+
+	//---CONEXION----//
+	// Dial a gRPC connection. The address to dial to can be configured when
+	// setting up the dgraph cluster.
+	d, err := grpc.Dial("localhost:9080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dgo.NewDgraphClient(
+		api.NewDgraphClient(d),
+	)
+
+}
