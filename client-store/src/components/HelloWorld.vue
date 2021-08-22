1586f71d192185047d53354c35819f61e8c90221
NatalyMora21 <tatamora21@gmail.com>
2021-08-22 12:54:28 -0500

    Created web interface folder

---
+++ client-store/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="hello">
+    </div>
+</template>
+
+<script>
+  import axios from "axios"
+  export default {
+    name: 'HelloWorld',
+    data(){
+      return 
+    },
+    async amounted() {
+      try {
+          var result = await axios({
+            method: "POST",
+            url: "http/localhost:3000"
+          })
+        
+      } catch (error) {
+
+        console.error(error)
+        
+      }
+  
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
